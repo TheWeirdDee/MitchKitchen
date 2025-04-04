e6636f2352db31fcfbfa77ad9bb3cf2ed9ebee58
Divine <your-email@divinenation1.com>
2025-04-04 23:14:49 +1400

    commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,25 +8,16 @@ import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <footer className="text-[#ce1126]">
-      <div className="bg-[#d3af37] py-5 max-w-9xl mx-auto flex flex-col md:flex-row items-center justify-center relative text-center">
+    <footer className="relative text-[#ce1126]">
+      <div className="bg-[#d3af37] py-5 max-w-9xl mx-auto flex items-stretch justify-between text-center min-h-[150px]">
         
         {/* Left Section */}
-        <div className="w-full Bona md:w-1/2 flex flex-col items-center">
-          <ul className="text-lg font-bold">
-          <Link to="/">
-            <li>HOME</li>
-            </Link>
-            <Link to="/gallery">
-            <li>MENU</li>
-            </Link>
-            <Link to="/about">
-            <li>ABOUT US</li>
-            </Link>
-            <Link to="/contact">
-            <li>CONTACT US</li>
-            </Link>
-             
+        <div className="w-1/2 flex flex-col items-center justify-center">
+          <ul className="text-lg Bona mt-2 font-extrabold flex flex-col gap-2">
+            <Link to="/"><li>HOME</li></Link>
+            <Link to="/gallery"><li>MENU</li></Link>
+            <Link to="/about"><li>ABOUT US</li></Link>
+            <Link to="/contact"><li>CONTACT US</li></Link>
           </ul>
           <img
             src={FooterLogo}
@@ -35,12 +26,12 @@ const Footer = () => {
           />
         </div>
 
-        {/* Demarcation Line */}
-        <div className="w-full md:w-[4px] md:h-full h-[4px] bg-[#013220] my-4 md:my-0 md:absolute md:left-1/2 md:top-0"></div>
+        {/* Demarcation Line (Now Stretches Full Height) */}
+        <div className="absolute left-1/2 top-0 bottom-0 w-[4px] bg-[#013220]"></div>
 
         {/* Right Section */}
-        <div className="playfair w-full md:w-1/2 flex flex-col items-center">
-          <h3 className="text-xl font-bold mb-3">LET'S CONNECT</h3>
+        <div className="playfair mb-20 w-1/2 flex flex-col items-center justify-center">
+          <h3 className="text-xl font-extrabold mb-3">LET'S CONNECT</h3>
           <div className="flex space-x-3">
             <a href="#" className="text-[#ce1126]">
               <AiFillInstagram className="w-8 h-8" />
