eb5868df58840eee7c936abf85e62ddb858b9fba
Divine <your-email@divinenation1.com>
2025-04-07 09:41:59 +1400

    commit

--- src/Components/SampleMenu.jsx
+++ src/Components/SampleMenu.jsx
@@ -5,7 +5,7 @@ export default function SampleMenu() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4 space-y-6">
       <div
-        className="w-full max-w-6xl bg-no-repeat mt-8 bg-cover bg-center h-[690px] md:h-[550px] lg:h-[550px] relative flex items-center justify-center"
+        className="w-full max-w-6xl bg-no-repeat mt-8 bg-cover bg-center h-[790px] md:h-[550px] lg:h-[550px] relative flex items-center justify-center"
         style={{ backgroundImage: `url(${Samplemenu})` }}
       >
         <div className="relative inset-0 flex flex-col items-center justify-center px-4 md:px-12 text-black perandory text-center md:translate-y-[-40px] lg:translate-y-[-60px]">
@@ -17,9 +17,9 @@ export default function SampleMenu() {
   <div className="lg:translate-y-[-20px] lg:mt-20 grid grid-cols-1 sm:grid-cols-2 sm:gap-4 md:mt-10  md:grid-cols-3 gap-6 text-base sm:text-lg w-full max-w-4xl">
   {/* STARTER */}
   <div className="mt-5 flex flex-col items-center sm:items-start">
-    <h2 className="font-bold text-xl mb-2">STARTER</h2>
+    <h2 className="font-bold text-3xl mb-6">STARTER</h2>
     <div className="flex flex-wrap justify-between w-full max-w-xs sm:block gap-2">
-      <p className="w-1/2 text-left sm:text-left">
+      <p className="text-left sm:text-left text-xl tracking-widest">
         Beef / Chicken Soup
       <br />
         with a choice of
@@ -31,9 +31,9 @@ export default function SampleMenu() {
 
   {/* MAIN */}
   <div className="md:border-x-2 border-dotted border-[#d5b981] px-4 flex flex-col items-center sm:items-start">
-    <h2 className="font-bold text-xl mb-2">MAIN</h2>
+    <h2 className="font-bold text-3xl mb-6">MAIN</h2>
     <div className="flex flex-wrap justify-between w-full max-w-xs sm:block gap-2">
-      <p className="w-1/2 text-left sm:text-left">
+      <p className=" text-left sm:text-left text-xl tracking-widest">
         Irish Meat Platter
       <br />
         with a choice of
@@ -45,9 +45,9 @@ export default function SampleMenu() {
 
   {/* DESSERTS */}
   <div className="sm:col-span-2 sm:mx-auto md:col-span-1 flex flex-col items-center">
-    <h2 className="font-bold text-xl mb-2">DESSERTS</h2>
+    <h2 className="font-bold text-3xl mb-6">DESSERTS</h2>
     <div className="flex flex-wrap justify-between w-full max-w-xs sm:block gap-2">
-      <p className=" text-left sm:text-left">
+      <p className="text-left sm:text-left text-xl tracking-widest">
         Guinness Punch Ice / Cream, with Chocolate Cake
       </p>
     </div>
